7e9555b871578903b3f0cbcf431ff091ce845544
Eli <eli.chandler@gmail.com>
2023-04-08 04:28:53 +1200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CapSolverPython',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Eli Chandler',
+    author_email='eli.chandler@gmail.com',
+    description='Python API for capsolver.com, supporting both sync and async usage!'
+)
